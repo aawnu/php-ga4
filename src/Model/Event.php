68f37b86f7da420dae3a25d077f98e7e7ed650fb
Alex Westergaard <aw.freelancer@gmail.com>
2022-07-12 22:02:18 +0200

    Check for reserved event names

--- src/Model/Event.php
+++ src/Model/Event.php
@@ -28,11 +28,36 @@ abstract class Event extends ToArray implements Interface\Export
         } else {
             $name = $this->getName();
             if (empty($name)) {
-                $errorStack = new GA4Exception("'self::getName()' can not be empty", $errorStack);
+                $errorStack = new GA4Exception("Name '{$name}' can not be empty", $errorStack);
             } elseif (strlen($name) > 40) {
-                $errorStack = new GA4Exception("'self::getName()' can not be longer than 40 characters", $errorStack);
+                $errorStack = new GA4Exception("Name '{$name}' can not be longer than 40 characters", $errorStack);
             } elseif (preg_match('/[^\w\d\-]/', $name)) {
-                $errorStack = new GA4Exception("'self::getName()' can only be letters, numbers, underscores and dashes", $errorStack);
+                $errorStack = new GA4Exception("Name '{$name}' can only be letters, numbers, underscores and dashes", $errorStack);
+            } elseif (in_array($name, [
+                'ad_activeview',
+                'ad_click',
+                'ad_exposure',
+                'ad_impression',
+                'ad_query',
+                'adunit_exposure',
+                'app_clear_data',
+                'app_install',
+                'app_update',
+                'app_remove',
+                'error',
+                'first_open',
+                'first_visit',
+                'in_app_purchase',
+                'notification_dismiss',
+                'notification_foreground',
+                'notification_open',
+                'notification_receive',
+                'os_update',
+                'screen_view',
+                'session_start',
+                'user_engagement',
+            ])) {
+                $errorStack = new GA4Exception("Name '{$name}' is reserved", $errorStack);
             } else {
                 $return['name'] = $name;
             }
