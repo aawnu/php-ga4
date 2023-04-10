32f7d2d45fda2acec236b00e9675b9a203dca6a4
Alex Westergaard <aw.freelancer@gmail.com>
2023-04-11 01:55:38 +0200

    Add expected variable for default event parameters

--- src/Facade/Type/DefaultEventParams.php
+++ src/Facade/Type/DefaultEventParams.php
@@ -4,10 +4,19 @@ namespace AlexWestergaard\PhpGa4\Facade\Type;
 
 interface DefaultEventParams
 {
+    /** @var language */
     public function setLanguage(string $lang);
+
+    /** @var page_location */
     public function setPageLocation(string $url);
+
+    /** @var page_referrer */
     public function setPageReferrer(string $url);
+
+    /** @var page_title */
     public function setPageTitle(string $title);
+
+    /** @var screen_resolution */
     public function setScreenResolution(string $wxh);
 
     public function toArray(): array;
