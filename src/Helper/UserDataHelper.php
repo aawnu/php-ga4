30b366f464ea313011b6070362c216a0500b0fa4
AAW <aaw@aaw.nu>
2024-07-02 04:47:34 +0200

    fix: wrapping up UserData as object

--- src/Helper/UserDataHelper.php
+++ src/Helper/UserDataHelper.php
@@ -4,18 +4,26 @@ namespace AlexWestergaard\PhpGa4\Helper;
 
 class UserDataHelper
 {
-    private array $sha256_email_address = [];
-    private array $sha256_phone_number = [];
+    private ?string $sha256_email_address = null;
+    private ?string $sha256_phone_number = null;
+
+    private ?string $sha256_first_name = null;
+    private ?string $sha256_last_name = null;
+    private ?string $sha256_street = null;
+    private ?string $city = null;
+    private ?string $region = null;
+    private ?string $postal_code = null;
+    private ?string $country = null;
 
     /**
      * @param string $email Valid email format
-     * @return int Cursor of array or -1 for error
+     * @return int Cursor of array or -1 for invalid
      */
-    public function addEmail(string $email): int
+    public function setEmail(string $email): bool
     {
         // Sanitize & Validate email
         $email = str_replace(" ", "", mb_strtolower($email));
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return -1; // Invalid email format
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) return false; // Invalid email format
 
         // Google Mail Sanitizer
         // https://support.google.com/mail/answer/7436150
@@ -28,38 +36,146 @@ class UserDataHelper
                 // https://support.google.com/mail/thread/125577450/gmail-and-googlemail?hl=en
                 $x[1] = "gmail.com";
             }
-            $x[0] = explode("+", $x[0], 2)[0]; // https://gmail.googleblog.com/2008/03/2-hidden-ways-to-get-more-from-your.html
+            // https://gmail.googleblog.com/2008/03/2-hidden-ways-to-get-more-from-your.html
+            $x[0] = explode("+", $x[0], 2)[0];
             $x[0] = str_replace(".", "", $x[0]);
             $email = implode("@", $x);
         }
 
         // Sha256 encode
-        $email = hash("sha256", $email);
-
-        // Skip duplicated and append email
-        $this->sha256_email_address = array_filter($this->sha256_email_address, fn ($v) => $v != $email);
-        return array_push($this->sha256_email_address, $email) - 1;
+        $this->sha256_email_address = hash("sha256", $email);
+        return true;
     }
 
     /**
-     * This converst prefix and number into international approved numbers
      * @param int $number fully international number (without dashes or plus) eg. \
      * "+1-123-4567890" for USA or\
      * "+44-1234-5678900" for UK or\
      * "+45-12345678" for DK
-     * @return int Cursor of array or -1 for error
+     * @return boolean
      */
-    public function addPhone(int $number): int
+    public function setPhone(int $number): bool
     {
-        $sNumber = (string)$number;
-        if (strlen($sNumber) < 1 || strlen($sNumber) > 15) {
-            return -1;
+        $sNumber = strval($number);
+        if (strlen($sNumber) < 3 || strlen($sNumber) > 15) {
+            return false;
         }
 
-        $sNumber = hash("sha256", "+{$sNumber}");
+        $this->sha256_phone_number = hash("sha256", "+{$sNumber}");
+        return true;
+    }
 
-        // Skip duplicated and append phone number
-        $this->sha256_phone_number = array_filter($this->sha256_phone_number, fn ($v) => $v != $sNumber);
-        return array_push($this->sha256_phone_number, $sNumber) - 1;
+    public function setFirstName(string $firstName): bool
+    {
+        if (empty($firstName)) return false;
+        $this->sha256_first_name = hash("sha256", $this->strip($firstName, true));
+        return true;
+    }
+
+    public function setLastName(string $lastName): bool
+    {
+        if (empty($lastName)) return false;
+        $this->sha256_last_name = hash("sha256", $this->strip($lastName, true));
+        return true;
+    }
+
+    public function setStreet(string $street): bool
+    {
+        if (empty($street)) return false;
+        $this->sha256_street = hash("sha256", $this->strip($street));
+        return true;
+    }
+
+    public function setCity(string $city): bool
+    {
+        if (empty($city)) return false;
+        $this->city = $this->strip($city, true);
+        return true;
+    }
+
+    public function setRegion(string $region): bool
+    {
+        if (empty($region)) return false;
+        $this->region = $this->strip($region, true);
+        return true;
+    }
+
+    public function setPostalCode(string $postalCode): bool
+    {
+        if (empty($postalCode)) return false;
+        $this->postal_code = $this->strip($postalCode);
+        return true;
+    }
+
+    public function setCountry(string $iso): bool
+    {
+        if (!CountryIsoHelper::valid($iso)) {
+            return false;
+        }
+
+        $this->country = mb_strtoupper(trim($iso));
+        return false;
+    }
+
+    public function toArray(): array
+    {
+        $res = [];
+
+        if (!empty($this->sha256_email_address)) {
+            $res["sha256_email_address"] = $this->sha256_email_address;
+        }
+
+        if (!empty($this->sha256_phone_number)) {
+            $res["sha256_phone_number"] = $this->sha256_phone_number;
+        }
+
+        $addr = [];
+
+        if (!empty($this->sha256_first_name)) {
+            $addr["sha256_first_name"] = $this->sha256_first_name;
+        }
+
+        if (!empty($this->sha256_last_name)) {
+            $addr["sha256_last_name"] = $this->sha256_last_name;
+        }
+
+        if (!empty($this->sha256_street)) {
+            $addr["sha256_street"] = $this->sha256_street;
+        }
+
+        if (!empty($this->city)) {
+            $addr["city"] = $this->city;
+        }
+
+        if (!empty($this->region)) {
+            $addr["region"] = $this->region;
+        }
+
+        if (!empty($this->postal_code)) {
+            $addr["postal_code"] = $this->postal_code;
+        }
+
+        if (!empty($this->country)) {
+            $addr["country"] = $this->country;
+        }
+
+        if (!empty($this->sha256_phone_number)) {
+            $res["sha256_phone_number"] = $this->sha256_phone_number;
+        }
+
+        if (count($addr) > 0) {
+            $res["address"] = $addr;
+        }
+
+        return $res;
+    }
+
+    private function strip(string $s, bool $removeDigits = false): string
+    {
+        $d = $removeDigits ? '0-9' : '';
+
+        $s = preg_replace("[^a-zA-Z{$d}\-\_\.\,\s]", "", $s);
+        $s = mb_strtolower($s);
+        return trim($s);
     }
 }
